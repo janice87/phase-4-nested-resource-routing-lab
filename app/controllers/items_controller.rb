e6964561ff2f7885393007a9346acc1d2d67b21e
Janice Chan <janice.chan1288@gmail.com>
2022-08-07 11:07:14 -0400

    commit tree 9994436973ea28870e5bcca032941e341b2a3937

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,40 @@
 class ItemsController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
 
+# Nested Items Index Route
+# GET /users/:user_id/items
   def index
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+    else   
     items = Item.all
+    end
     render json: items, include: :user
   end
 
+# Nested Item Show Route
+# GET /users/:user_id/items/:id
+# no change bc we are rendering a single item, regardless of how it was accessed
+  def show   
+    item = Item.find(params[:id]) # just id bc that is the primay key??
+    render json: item
+  end
+
+# Nested Item Create Route
+# POST /users/:user_id/items
+  def create    
+    user = User.find(params[:user_id])
+    item = user.items.create(items_params)
+    render json: item, status: :created    
+  end
+
+  private
+  def render_not_found
+    render json: {error: "Record not found"}, status: :not_found
+  end
+
+  def items_params
+    params.permit(:name, :price, :description)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
   resources :items, only: [:index]
-  resources :users, only: [:show]
+
+  resources :users, only: [:show] do 
+    resources :items, only: [:index, :show, :create]
+  end
 end
+
+# Nested Items Index Route
+# GET /users/:user_id/items
+
+# Nested Item Show Route
+# GET /users/:user_id/items/:id
+
+# Nested Item Create Route
+# POST /users/:user_id/items
